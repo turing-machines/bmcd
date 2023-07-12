6ab3f2008f88436ef83ceccbae618b67d6b7669b
Sven Rademakers <sven.rademakers@gmail.com>
2023-07-12 16:49:48 +0100

    Implemented RK1 fwupdate support.
    This work fixes #45:
        https://github.com/turing-machines/BMC-Firmware/issues/45
    
    Implemented discovery of an RK1 module as well as Implemented the
    firmware update read/write trait. Note that its implemented in a
    blocking way. #12 is opened to track any work to improve on this.
    
    This implementation expects the RK1 to be put into maskedROM mode.
    Writing to eMMC starts from sector 0.

--- src/middleware/rk1_fwudate.rs
+++ src/middleware/rk1_fwudate.rs
@@ -1,25 +1,62 @@
-#![allow(unused_variables)]
+use crate::middleware::usbboot::FlashStatus;
+
 use super::{
     firmware_update_usb::FwUpdate,
     usbboot::{FlashProgress, FlashingError},
 };
+use bytes::BufMut;
 use core::{
     pin::Pin,
-    task::{Context, Poll},
+    task::{self, Poll},
 };
-use std::io::Error;
+use rockusb::libusb::{Devices, Transport, TransportIO};
+use std::io::{Error, Write};
 use tokio::{
     io::{AsyncRead, AsyncWrite, ReadBuf},
     sync::mpsc::Sender,
 };
 
-#[derive(Debug)]
-pub struct Rk1FwUpdateDriver {}
+pub struct Rk1FwUpdateDriver {
+    transport: TransportIO<Transport>,
+}
 
 impl Rk1FwUpdateDriver {
     pub const VID_PID: (u16, u16) = (0x2207, 0x350b);
     pub fn new(logging: Sender<FlashProgress>) -> Result<Self, FlashingError> {
-        todo!()
+        let dev_not_found = || {
+            let _ = logging.try_send(FlashProgress {
+                status: FlashStatus::Error(FlashingError::DeviceNotFound),
+                message: "could not find a connected RK1".to_string(),
+            });
+            FlashingError::DeviceNotFound
+        };
+
+        let io_error = |e| {
+            let _ = logging.try_send(FlashProgress {
+                status: FlashStatus::Error(FlashingError::IoError),
+                message: format!("{}", e),
+            });
+            FlashingError::IoError
+        };
+
+        let devices = Devices::new().map_err(|_| FlashingError::IoError)?;
+        let mut transport = devices
+            .iter()
+            .next()
+            .ok_or_else(dev_not_found)?
+            .map_err(|_| FlashingError::UsbError)?;
+
+        let _ = logging.try_send(FlashProgress {
+            status: FlashStatus::Setup,
+            message: format!(
+                "Chip Info: {:0x?}",
+                transport.chip_info().map_err(|_| FlashingError::UsbError)?
+            ),
+        });
+
+        Ok(Self {
+            transport: transport.into_io().map_err(io_error)?,
+        })
     }
 }
 
@@ -28,27 +65,36 @@ impl FwUpdate for Rk1FwUpdateDriver {}
 impl AsyncRead for Rk1FwUpdateDriver {
     fn poll_read(
         self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
+        _: &mut task::Context<'_>,
         buf: &mut ReadBuf<'_>,
     ) -> Poll<std::io::Result<()>> {
-        todo!()
+        //TODO: upgrade implementation to an async variant.
+        let mut writer = buf.writer();
+        let this = self.get_mut();
+        let result = std::io::copy(&mut this.transport, &mut writer).map(|_| ());
+        Poll::Ready(result)
     }
 }
 
 impl AsyncWrite for Rk1FwUpdateDriver {
     fn poll_write(
         self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
+        _: &mut task::Context<'_>,
         buf: &[u8],
     ) -> Poll<Result<usize, Error>> {
-        todo!()
+        //TODO: upgrade implementation to an async variant.
+        let result = self.get_mut().transport.write(buf);
+        Poll::Ready(result)
     }
 
-    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Error>> {
-        todo!()
+    fn poll_flush(self: Pin<&mut Self>, _: &mut task::Context<'_>) -> Poll<Result<(), Error>> {
+        //TODO: upgrade implementation to an async variant.
+        let result = self.get_mut().transport.flush();
+        Poll::Ready(result)
     }
 
-    fn poll_shutdown(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Error>> {
-        todo!()
+    fn poll_shutdown(self: Pin<&mut Self>, _: &mut task::Context<'_>) -> Poll<Result<(), Error>> {
+        //TODO: upgrade implementation to an async variant.
+        Poll::Ready(Ok(()))
     }
 }
