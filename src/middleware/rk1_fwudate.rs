5fca751e64a9d02c0902452d425c7c9518a91f68
Sven Rademakers <sven.rademakers@gmail.com>
2023-07-12 16:49:48 +0100

    rk1_fwudate: add device discovery

--- src/middleware/rk1_fwudate.rs
+++ src/middleware/rk1_fwudate.rs
@@ -1,25 +1,52 @@
-#![allow(unused_variables)]
+#![allow(unused_variables, dead_code)]
+use crate::middleware::usbboot::FlashStatus;
+
 use super::{
     firmware_update_usb::FwUpdate,
     usbboot::{FlashProgress, FlashingError},
 };
 use core::{
     pin::Pin,
-    task::{Context, Poll},
+    task::{self, Poll},
 };
+use rockusb::libusb::{Devices, Transport};
 use std::io::Error;
 use tokio::{
     io::{AsyncRead, AsyncWrite, ReadBuf},
     sync::mpsc::Sender,
 };
 
-#[derive(Debug)]
-pub struct Rk1FwUpdateDriver {}
+pub struct Rk1FwUpdateDriver {
+    transport: Transport,
+}
 
 impl Rk1FwUpdateDriver {
     pub const VID_PID: (u16, u16) = (0x2207, 0x350b);
     pub fn new(logging: Sender<FlashProgress>) -> Result<Self, FlashingError> {
-        todo!()
+        let dev_not_found = || {
+            let _ = logging.try_send(FlashProgress {
+                status: FlashStatus::Error(FlashingError::DeviceNotFound),
+                message: "could not find a connected RK1".to_string(),
+            });
+            FlashingError::DeviceNotFound
+        };
+
+        let devices = Devices::new().map_err(|_| FlashingError::IoError)?;
+        let mut transport = devices
+            .iter()
+            .next()
+            .ok_or_else(dev_not_found)?
+            .map_err(|_| FlashingError::UsbError)?;
+
+        let _ = logging.try_send(FlashProgress {
+            status: FlashStatus::Setup,
+            message: format!(
+                "Chip Info: {:0x?}",
+                transport.chip_info().map_err(|_| FlashingError::UsbError)?
+            ),
+        });
+
+        Ok(Self { transport })
     }
 }
 
@@ -28,7 +55,7 @@ impl FwUpdate for Rk1FwUpdateDriver {}
 impl AsyncRead for Rk1FwUpdateDriver {
     fn poll_read(
         self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
+        cx: &mut task::Context<'_>,
         buf: &mut ReadBuf<'_>,
     ) -> Poll<std::io::Result<()>> {
         todo!()
@@ -38,17 +65,17 @@ impl AsyncRead for Rk1FwUpdateDriver {
 impl AsyncWrite for Rk1FwUpdateDriver {
     fn poll_write(
         self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
+        cx: &mut task::Context<'_>,
         buf: &[u8],
     ) -> Poll<Result<usize, Error>> {
         todo!()
     }
 
-    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Error>> {
+    fn poll_flush(self: Pin<&mut Self>, cx: &mut task::Context<'_>) -> Poll<Result<(), Error>> {
         todo!()
     }
 
-    fn poll_shutdown(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Error>> {
+    fn poll_shutdown(self: Pin<&mut Self>, cx: &mut task::Context<'_>) -> Poll<Result<(), Error>> {
         todo!()
     }
 }
