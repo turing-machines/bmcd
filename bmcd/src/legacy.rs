95645ae2a23da6cc52e01b3075ddedaf14076155
Ruslan Akbashev <ra@turingpi.com>
2023-08-02 21:22:38 +0400

    bmcd/legacy: add GET methods

--- bmcd/src/legacy.rs
+++ bmcd/src/legacy.rs
@@ -1,8 +1,42 @@
 //! Routes for legacy API present in versions <= 1.0.2 of the firmware.
 
-use actix_web::{web, HttpResponse};
+use std::fmt::Display;
+use std::str::FromStr;
+use std::sync::Arc;
+
+use actix_web::{rt, web, HttpResponse};
+use nix::sys::statfs::statfs;
+use serde_json::json;
+use tokio::sync::{mpsc, Mutex};
+use tpi_rs::app::bmc_application::{BmcApplication, UsbConfig};
+use tpi_rs::middleware::{NodeId, UsbMode, UsbRoute};
 
 type Query = web::Query<std::collections::HashMap<String, String>>;
+
+trait ToHttpResponse {
+    fn to_response(&self, action: &'static str) -> HttpResponse;
+}
+
+impl<T, E: Display> ToHttpResponse for Result<T, E> {
+    fn to_response(&self, action: &'static str) -> HttpResponse {
+        match self {
+            Ok(_) => success_response(),
+            Err(e) => {
+                let msg = format!("Failed to {}: {}", action, e);
+                HttpResponse::InternalServerError().body(msg)
+            }
+        }
+    }
+}
+
+fn success_response() -> HttpResponse {
+    // For backwards compatibility, `response` holds an array, even though it's unnecessary
+    let msg = json!({
+        "response": [{ "result": "ok" }]
+    });
+
+    HttpResponse::Ok().json(msg)
+}
 
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(
@@ -14,7 +48,8 @@ pub fn config(cfg: &mut web::ServiceConfig) {
     );
 }
 
-async fn api_entry(query: Query) -> HttpResponse {
+// TODO: BmcApplication::new() needs to return just BmcApplication, not wrapped in Arc<>.
+async fn api_entry(bmc: web::Data<Mutex<Arc<BmcApplication>>>, query: Query) -> HttpResponse {
     let is_set = match query.get("opt").cloned().as_deref() {
         Some("set") => true,
         Some("get") => false,
@@ -25,23 +60,298 @@ async fn api_entry(query: Query) -> HttpResponse {
         return HttpResponse::BadRequest().body("Missing `type` parameter");
     };
 
+    let bmc = &mut bmc.lock().await;
+
     match (ty.as_ref(), is_set) {
-        ("clear_usb_boot", true) => stub(),
-        ("network", true) => stub(),
-        ("nodeinfo", true) => stub(),
-        ("nodeinfo", false) => stub(),
-        ("node_to_msd", true) => stub(),
-        ("other", true) => stub(),
-        ("power", true) => stub(),
-        ("power", false) => stub(),
-        ("sdcard", true) => stub(),
-        ("sdcard", false) => stub(),
-        ("uart", true) => stub(),
-        ("uart", false) => stub(),
-        ("usb", true) => stub(),
-        ("usb", false) => stub(),
+        ("clear_usb_boot", true) => clear_usb_boot(bmc),
+        ("network", true) => reset_network(bmc).await,
+        ("nodeinfo", true) => set_node_info(),
+        ("nodeinfo", false) => get_node_info(bmc),
+        ("node_to_msd", true) => set_node_to_msd(bmc, query).await,
+        ("other", false) => get_system_information().await,
+        ("power", true) => set_node_power(bmc, query).await,
+        ("power", false) => get_node_power(bmc).await,
+        ("sdcard", true) => format_sdcard(),
+        ("sdcard", false) => get_sdcard_info(),
+        ("uart", true) => write_to_uart(bmc, query),
+        ("uart", false) => read_from_uart(bmc, query),
+        ("usb", true) => set_usb_mode(bmc, query).await,
+        ("usb", false) => get_usb_mode(bmc),
         _ => HttpResponse::BadRequest().body("Invalid `type` parameter"),
     }
+}
+
+fn clear_usb_boot(bmc: &mut Arc<BmcApplication>) -> HttpResponse {
+    bmc.clear_usb_boot().to_response("clear USB boot mode")
+}
+
+async fn reset_network(bmc: &mut Arc<BmcApplication>) -> HttpResponse {
+    bmc.rtl_reset().await.to_response("reset network switch")
+}
+
+fn set_node_info() -> HttpResponse {
+    // In previous versions of the firmware this was dead code
+    HttpResponse::NotImplemented().body("Method type `set` on parameter `nodeinfo` is deprecated")
+}
+
+fn get_node_info(_bmc: &mut Arc<BmcApplication>) -> HttpResponse {
+    // TODO: implement serial listening in BmcApplication
+    let (n1, n2, n3, n4) = (0, 0, 0, 0);
+
+    let body = json!({
+        "response": [{
+            "node1": n1,
+            "node2": n2,
+            "node3": n3,
+            "node4": n4,
+        }]
+    });
+
+    HttpResponse::Ok().json(body)
+}
+
+async fn set_node_to_msd(bmc: &mut Arc<BmcApplication>, query: Query) -> HttpResponse {
+    let node = match get_node_param(&query) {
+        Ok(n) => n,
+        Err(e) => return HttpResponse::BadRequest().body(e),
+    };
+
+    let (tx, mut rx) = mpsc::channel(64);
+
+    let bmc = bmc.clone();
+    let handle = rt::spawn(async move { bmc.set_node_in_msd(node, UsbRoute::Bmc, tx).await });
+
+    let logger = rt::spawn(async move {
+        while let Some(msg) = rx.recv().await {
+            log::info!("{}", msg);
+        }
+    });
+
+    tokio::try_join!(handle, logger).to_response("await threads")
+}
+
+fn get_node_param(query: &Query) -> Result<NodeId, &'static str> {
+    let Some(node_str) = query.get("node") else {
+        return Err("Missing `node` parameter");
+    };
+
+    let Ok(node_num) = i32::from_str(node_str) else {
+        return Err("Parameter `node` is not a number");
+    };
+
+    let Ok(node) = node_num.try_into() else {
+        return Err("Parameter `node` is out of range 0..3 of node IDs");
+    };
+
+    Ok(node)
+}
+
+async fn get_system_information() -> HttpResponse {
+    let version = env!("CARGO_PKG_VERSION");
+    let build_time = build_time::build_time_utc!("%Y-%m-%d %H:%M:%S-00:00");
+    let ipv4 = get_ipv4_address().unwrap_or("Unknown".to_owned());
+    let mac = get_mac_address().await;
+
+    let body = json!({
+        "response": [{
+            "version": version,
+            "buildtime": build_time,
+            "ip": ipv4,
+            "mac": mac,
+        }]
+    });
+
+    HttpResponse::Ok().json(body)
+}
+
+fn get_ipv4_address() -> Option<String> {
+    for interface in if_addrs::get_if_addrs().ok()? {
+        // TODO: for compatibility reasons, only IPv4 of eth0 is returned. Ideally, both IPv4 and
+        // IPv6 addresses of all non-loopback interfaces should be returned.
+        if interface.is_loopback() || interface.name != "eth0" {
+            continue;
+        }
+
+        let std::net::IpAddr::V4(ip) = interface.ip() else {
+            continue;
+        };
+
+        return Some(format!("{}", ip));
+    }
+
+    None
+}
+
+async fn get_mac_address() -> String {
+    tokio::fs::read_to_string("/sys/class/net/eth0/address")
+        .await
+        .unwrap_or("Unknown".to_owned())
+}
+
+async fn set_node_power(bmc: &mut Arc<BmcApplication>, query: Query) -> HttpResponse {
+    for id in 0..4 {
+        let param = format!("node{}", id);
+        let req_status = match query.get(&param).cloned().as_deref() {
+            Some("0") => false,
+            Some("1") => true,
+            Some(x) => {
+                let msg = format!("Invalid value `{}` for parameter `{}`", x, param);
+                return HttpResponse::BadRequest().body(msg);
+            }
+            None => continue,
+        };
+        // Unwrap: values of the loop are statically bound to cover only allowed NodeId values
+        let node = NodeId::try_from(id).unwrap();
+        let curr_status = match bmc.get_node_power(node).await {
+            Ok(s) => s,
+            Err(e) => {
+                let msg = format!("Failed to get power-on status of node {}: {}", id, e);
+                return HttpResponse::InternalServerError().body(msg);
+            }
+        };
+
+        if curr_status != req_status {
+            let states = if req_status {
+                node.to_bitfield()
+            } else {
+                node.to_inverse_bitfield()
+            };
+            let mask = 1 << id;
+
+            match bmc.activate_slot(states, mask).await {
+                Ok(_) => continue,
+                Err(e) => {
+                    let desc = if req_status { "on" } else { "off" };
+                    let msg = format!("Failed to power {} node {}: {}", desc, id + 1, e);
+                    return HttpResponse::InternalServerError().body(msg);
+                }
+            }
+        }
+    }
+
+    success_response()
+}
+
+async fn get_node_power(bmc: &mut Arc<BmcApplication>) -> HttpResponse {
+    let n1 = get_node_id_power_status(bmc, 0).await;
+    let n2 = get_node_id_power_status(bmc, 1).await;
+    let n3 = get_node_id_power_status(bmc, 2).await;
+    let n4 = get_node_id_power_status(bmc, 3).await;
+
+    let body = json!({
+        "response": [{
+            "node1": n1,
+            "node2": n2,
+            "node3": n3,
+            "node4": n4,
+        }]
+    });
+
+    HttpResponse::Ok().json(body)
+}
+
+async fn get_node_id_power_status(bmc: &mut Arc<BmcApplication>, id: i32) -> String {
+    // Unwrap: `id` values are statically defined to cover only allowed NodeId values
+    let node = NodeId::try_from(id).unwrap();
+    let Ok(status) = bmc.get_node_power(node).await else {
+        return "Unknown".to_owned();
+    };
+
+    u8::from(status).to_string()
+}
+
+fn format_sdcard() -> HttpResponse {
+    HttpResponse::NotImplemented().body("microSD card formatting is not implemented")
+}
+
+fn get_sdcard_info() -> HttpResponse {
+    match get_sdcard_fs_stat() {
+        Ok((total, free)) => {
+            let used = total - free;
+            let body = json!({
+                "response": [{
+                    "total": total,
+                    "use": used,
+                    "free": free,
+                }]
+            });
+
+            HttpResponse::Ok().json(body)
+        }
+        Err(e) => {
+            let msg = format!("Failed to get microSD card info: {}", e);
+            HttpResponse::InternalServerError().body(msg)
+        }
+    }
+}
+
+fn get_sdcard_fs_stat() -> anyhow::Result<(u64, u64)> {
+    let stat = statfs("/mnt/sdcard")?;
+    let bsize = u64::try_from(stat.block_size())?;
+    let total = bsize * stat.blocks();
+    let free = bsize * stat.blocks_available();
+
+    Ok((total, free))
+}
+
+fn write_to_uart(bmc: &mut Arc<BmcApplication>, query: Query) -> HttpResponse {
+    let node = match get_node_param(&query) {
+        Ok(n) => n,
+        Err(e) => return HttpResponse::BadRequest().body(e),
+    };
+
+    let Some(cmd) = query.get("cmd") else {
+        return HttpResponse::BadRequest().body("Missing `cmd` parameter");
+    };
+
+    uart_write(bmc, node, cmd).to_response("write over UART")
+}
+
+fn uart_write(_bmc: &mut Arc<BmcApplication>, _node: NodeId, _cmd: &str) -> anyhow::Result<()> {
+    todo!()
+}
+
+fn read_from_uart(bmc: &mut Arc<BmcApplication>, query: Query) -> HttpResponse {
+    let node = match get_node_param(&query) {
+        Ok(n) => n,
+        Err(e) => return HttpResponse::BadRequest().body(e),
+    };
+
+    uart_read(bmc, node).to_response("read from UART")
+}
+
+fn uart_read(_bmc: &mut Arc<BmcApplication>, _node: NodeId) -> anyhow::Result<()> {
+    todo!()
+}
+
+async fn set_usb_mode(bmc: &mut Arc<BmcApplication>, query: Query) -> HttpResponse {
+    let node = match get_node_param(&query) {
+        Ok(n) => n,
+        Err(e) => return HttpResponse::BadRequest().body(e),
+    };
+
+    let Some(mode_str) = query.get("mode") else {
+        return HttpResponse::BadRequest().body("Missing `mode` parameter");
+    };
+
+    let Ok(mode_num) = i32::from_str(mode_str) else {
+        return HttpResponse::BadRequest().body("Parameter `mode` is not a number");
+    };
+
+    let Ok(mode) = mode_num.try_into() else {
+        return HttpResponse::BadRequest().body("Parameter `mode` can be either 0 or 1");
+    };
+
+    let cfg = match mode {
+        UsbMode::Device => UsbConfig::UsbA(node, true),
+        UsbMode::Host => UsbConfig::Node(node, UsbRoute::UsbA),
+    };
+
+    bmc.configure_usb(cfg).await.to_response("set USB mode")
+}
+
+fn get_usb_mode(_bmc: &mut Arc<BmcApplication>) -> HttpResponse {
+    todo!()
 }
 
 async fn api_post(query: Query) -> HttpResponse {
@@ -56,7 +366,7 @@ async fn api_post(query: Query) -> HttpResponse {
     match ty.as_ref() {
         "firmware" => stub(),
         "flash" => stub(),
-        _ => return HttpResponse::BadRequest().body("Invalid `type` parameter"),
+        _ => HttpResponse::BadRequest().body("Invalid `type` parameter"),
     }
 }
 
--- bmcd/src/main.rs
+++ bmcd/src/main.rs
@@ -1,23 +1,35 @@
 use actix_files::Files;
-use actix_web::{middleware, App, HttpServer};
+use actix_web::{middleware, web::Data, App, HttpServer};
 use log::LevelFilter;
+use tokio::sync::Mutex;
+use tpi_rs::app::bmc_application::BmcApplication;
 
 mod legacy;
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> anyhow::Result<()> {
     init_logger();
 
-    HttpServer::new(|| {
+    let bmc = Mutex::new(BmcApplication::new().await?);
+    let bmc_shared = Data::new(bmc);
+
+    HttpServer::new(move || {
         App::new()
+            // Shared state: mutex of BmcApplication instance
+            .app_data(bmc_shared.clone())
+            // Legacy API
             .configure(legacy::config)
+            // Enable logger
             .wrap(middleware::Logger::default())
             // Serve a static tree of files of the web UI. Must be the last item.
             .service(Files::new("/", "/mnt/var/www/").index_file("index.html"))
     })
     .bind(("0.0.0.0", 8080))?
+    .workers(2)
     .run()
-    .await
+    .await?;
+
+    Ok(())
 }
 
 fn init_logger() {
