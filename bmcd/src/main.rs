d9200b82bb3a168738bfee8eda7edf79585d5408
Ruslan Akbashev <ra@turingpi.com>
2023-08-02 21:16:15 +0400

    add HTTPS server that serves static files

--- bmcd/src/main.rs
+++ bmcd/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello from bmcd!");
+use actix_files::Files;
+use actix_web::{middleware, App, HttpServer};
+use log::LevelFilter;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    init_logger();
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::Logger::default())
+            // Serve a static tree of files of the web UI. Must be the last item.
+            .service(Files::new("/", "/mnt/var/www/").index_file("index.html"))
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
+}
+
+fn init_logger() {
+    let level = if cfg!(debug_assertions) {
+        LevelFilter::Debug
+    } else {
+        LevelFilter::Warn
+    };
+
+    simple_logger::SimpleLogger::new()
+        .with_level(level)
+        .with_module_level("bmcd", LevelFilter::Info)
+        .with_colors(true)
+        .env()
+        .init()
+        .expect("failed to initialize logger");
+
+    log::info!("Turing Pi 2 BMC Daemon v{}", env!("CARGO_PKG_VERSION"));
 }
