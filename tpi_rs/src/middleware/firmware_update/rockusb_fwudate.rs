a9e73f3deeee99217f7531b020b99ceb28ae5222
Sven Rademakers <sven.rademakers@gmail.com>
2023-08-14 16:32:32 +0100

    firmware_update: FlashingErrorExt
    Created trait that maps a `Result<T, E>` into a
    `Result<T, FlashingError>` using the logging pipe.
    
    This commit aims to increase code clarity by concentrating generic
    logic.

--- tpi_rs/src/middleware/firmware_update/rockusb_fwudate.rs
+++ tpi_rs/src/middleware/firmware_update/rockusb_fwudate.rs
@@ -1,5 +1,5 @@
 use super::transport::{StdFwUpdateTransport, StdTransportWrapper};
-use crate::middleware::usbboot::{FlashProgress, FlashStatus, FlashingError};
+use crate::middleware::usbboot::{FlashProgress, FlashingError, FlashingErrorExt};
 use rockusb::libusb::{Transport, TransportIO};
 use rusb::GlobalContext;
 use tokio::sync::mpsc::Sender;
@@ -7,29 +7,13 @@ use tokio::sync::mpsc::Sender;
 pub const RK3588_VID_PID: (u16, u16) = (0x2207, 0x350b);
 pub async fn new_rockusb_transport(
     device: rusb::Device<GlobalContext>,
-    logging: Sender<FlashProgress>,
+    logging: &Sender<FlashProgress>,
 ) -> Result<StdTransportWrapper<TransportIO<Transport>>, FlashingError> {
-    let usb_error = |e| {
-        let _ = logging.try_send(FlashProgress {
-            status: FlashStatus::Error(FlashingError::UsbError),
-            message: format!("{}", e),
-        });
-        FlashingError::UsbError
-    };
-
-    let io_error = |e| {
-        let _ = logging.try_send(FlashProgress {
-            status: FlashStatus::Error(FlashingError::IoError),
-            message: format!("{}", e),
-        });
-        FlashingError::IoError
-    };
-
-    let transport = Transport::from_usb_device(device.open().map_err(usb_error)?)
+    let transport = Transport::from_usb_device(device.open().map_err_into_logged_usb(logging)?)
         .map_err(|_| FlashingError::UsbError)?;
 
     Ok(StdTransportWrapper::new(
-        transport.into_io().map_err(io_error)?,
+        transport.into_io().map_err_into_logged_io(logging)?,
     ))
 }
 
