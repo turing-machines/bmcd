b7dd93678c80ae096307f3663e3f2bc73af59d75
Sven Rademakers <sven.rademakers@gmail.com>
2023-08-30 13:55:46 +0100

    FlashService: fixes

--- tpi_rs/src/utils/mod.rs
+++ tpi_rs/src/utils/mod.rs
@@ -1,5 +1,20 @@
 mod event_listener;
+use std::fmt::Display;
+
 #[doc(inline)]
 pub use event_listener::*;
 mod io;
 pub use io::*;
+use tokio::sync::mpsc::Receiver;
+
+// for now we print the status updates to console. In the future we would like to pass
+// this back to the clients.
+pub fn logging_sink<T: Display + Send + 'static>(
+    mut receiver: Receiver<T>,
+) -> tokio::task::JoinHandle<()> {
+    tokio::spawn(async move {
+        while let Some(msg) = receiver.recv().await {
+            log::info!("{}", msg);
+        }
+    })
+}
