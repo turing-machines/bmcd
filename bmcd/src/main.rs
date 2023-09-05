a8c05796e0a2ef0ce9afd6960143292be9b9d1f4
Sven Rademakers <sven.rademakers@gmail.com>
2023-09-05 20:52:26 +0100

    enable tls
    TLS is added and enforced. the bmcd requires a `--certificate` and
    `--private-key` argument in order to run.
    
    A server is binded onto the old `80` port, that will redirect traffic to
    443

--- bmcd/src/main.rs
+++ bmcd/src/main.rs
@@ -1,25 +1,45 @@
 use crate::flash_service::FlashService;
 use actix_files::Files;
-use actix_web::{http::KeepAlive, middleware, web::Data, App, HttpServer};
+use actix_web::{
+    http::{self, KeepAlive},
+    middleware, web,
+    web::Data,
+    App, HttpRequest, HttpResponse, HttpServer,
+};
+use anyhow::Context;
+use clap::{command, value_parser, Arg};
 use log::LevelFilter;
-use std::sync::Arc;
+use openssl::ssl::SslAcceptorBuilder;
+use std::{
+    path::{Path, PathBuf},
+    sync::Arc,
+};
 use tokio::sync::Mutex;
 use tpi_rs::app::{bmc_application::BmcApplication, event_application::run_event_listener};
+pub mod config;
 mod flash_service;
 mod into_legacy_response;
 mod legacy;
+use crate::config::Config;
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
+
+const HTTPS_PORT: u16 = 443;
+const HTTP_PORT: u16 = 80;
 
 #[actix_web::main]
 async fn main() -> anyhow::Result<()> {
     init_logger();
 
+    let config = Config::try_from(config_path()).context("Error parsing config file")?;
+    let tls = load_tls_configuration(&config.tls.private_key, &config.tls.certificate)?;
+    let tls6 = load_tls_configuration(&config.tls.private_key, &config.tls.certificate)?;
+
     let bmc = Arc::new(BmcApplication::new().await?);
     run_event_listener(bmc.clone())?;
-
     let flash_service = Data::new(Mutex::new(FlashService::new(bmc.clone())));
     let bmc = Data::from(bmc);
 
-    HttpServer::new(move || {
+    let run_server = HttpServer::new(move || {
         App::new()
             // Shared state: BmcApplication instance
             .app_data(bmc.clone())
@@ -31,15 +51,29 @@ async fn main() -> anyhow::Result<()> {
             // Serve a static tree of files of the web UI. Must be the last item.
             .service(Files::new("/", "/mnt/var/www/").index_file("index.html"))
     })
-    .bind(("0.0.0.0", 80))?
-    .bind(("::1", 80))?
+    .bind_openssl(("0.0.0.0", HTTPS_PORT), tls)?
+    .bind_openssl(("::1", HTTPS_PORT), tls6)?
     .keep_alive(KeepAlive::Os)
     .workers(2)
-    .run()
-    .await?;
+    .run();
 
+    // redirect requests to 'HTTPS'
+    let redirect_server = HttpServer::new(move || App::new().route("/", web::get().to(redirect)))
+        .bind(("0.0.0.0", HTTP_PORT))?
+        .run();
+
+    tokio::try_join!(run_server, redirect_server)?;
     log::info!("exiting {}", env!("CARGO_PKG_NAME"));
     Ok(())
+}
+
+async fn redirect(request: HttpRequest) -> HttpResponse {
+    let host = request.connection_info().host().to_string();
+    let path = request.uri().to_string();
+    let redirect_url = format!("https://{}{}", host, path);
+    HttpResponse::PermanentRedirect()
+        .append_header((http::header::LOCATION, redirect_url))
+        .finish()
 }
 
 fn init_logger() {
@@ -59,4 +93,28 @@ fn init_logger() {
         .expect("failed to initialize logger");
 
     log::info!("Turing Pi 2 BMC Daemon v{}", env!("CARGO_PKG_VERSION"));
+}
+
+fn config_path() -> PathBuf {
+    command!()
+        .arg(
+            Arg::new("config")
+                .long("config")
+                .value_parser(value_parser!(PathBuf))
+                .required(true),
+        )
+        .get_matches()
+        .get_one::<PathBuf>("config")
+        .expect("`config` argument required")
+        .into()
+}
+
+fn load_tls_configuration<P: AsRef<Path>>(
+    private_key: P,
+    certificate: P,
+) -> anyhow::Result<SslAcceptorBuilder> {
+    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls())?;
+    builder.set_private_key_file(private_key.as_ref(), SslFiletype::PEM)?;
+    builder.set_certificate_chain_file(certificate.as_ref())?;
+    Ok(builder)
 }
